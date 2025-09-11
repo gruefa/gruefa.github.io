a0e73fde11c39c3138ad3877e5b148904b6d92a3
Fabius Grünhagen <gruefa@gmx.de>
2025-09-11 22:43:31 +0200

    Rework Webpage layout, Refactor CSS, Add links to socials

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -3,60 +3,25 @@ function getCSSVariableColor(variableName) {
     return getComputedStyle(document.documentElement).getPropertyValue(variableName).trim();
 }
 
-function getResponsiveDimensions() {
-    const width = window.innerWidth;
-    if (width > 1200) {
-        return { width: Math.round(width / 8), height: 40, cellSize: 8 };
-    } else {
-        return { width: 150, height: Math.round(220 * 150 / width), cellSize: 8 };
-    }
-}
-
-function getTextMask(text, width, height, cell_size) {
-    // Draw text on offscreen canvas and get pixel data
-    const canvas = document.createElement("canvas");
-    canvas.width = width * cell_size;
-    canvas.height = height * cell_size;
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.font = `${cell_size * 26}px monospace`;
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText(text, canvas.width / 2, canvas.height / 2);
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
-
-    // Create mask based on alpha channel
-    let mask = Array.from({ length: height }, () => Array(width).fill(0));
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            let pixelIndex = ((y * cell_size) * canvas.width + (x * cell_size)) * 4;
-            let alpha = data[pixelIndex + 3];
-            if (alpha > 128) {
-                mask[y][x] = 1;
-            }
-        }
-    }
-    return mask;
+function getResponsiveDimensions(canvas) {
+    const width = document.body.clientWidth;
+    const height = 400; // Fixed height for banner
+    return { width: Math.ceil(width / 5), height: Math.ceil(height / 5), cellSize: 5 };
 }
 
 class GameOfLife {
-    load(canvas, text) {
-        const dims = getResponsiveDimensions();
+    load(canvas) {
+        const dims = getResponsiveDimensions(canvas);
         this.width = dims.width;
         this.height = dims.height;
         this.cell_size = dims.cellSize;
         this.grid = Array.from({ length: this.height }, () => Array(this.width).fill(0));
-        this.mask = getTextMask(text.innerHTML || "Hello!", this.width, this.height, this.cell_size);
 
         // Update canvas size
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
         this.canvas.width = this.width * this.cell_size;
         this.canvas.height = this.height * this.cell_size;
-
-        // Set text invisible
-        text.style.color = "#fff0";
 
         // Randomly initialize the grid
         for (let y = 0; y < this.height; y++) {
@@ -106,26 +71,19 @@ class GameOfLife {
 
     draw() {
         // Get current theme colors
-        let colorAliveMask = getCSSVariableColor('--accent-color-2') || "darkred";
-        let colorAlive = getCSSVariableColor('--accent-color-1') || "orangered";
-        let colorDeadMask = getCSSVariableColor('--text-color') || "#8888af";
-        let colorDead = getCSSVariableColor('--bg-color-2') || "#eeeef8";
+        let colorAlive = getCSSVariableColor('--accent-color-1') || "black";
+        let colorDead = getCSSVariableColor('--bg-color-1') || "white";
 
         // Draw cells
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         for (let y = 0; y < this.height; y++) {
             for (let x = 0; x < this.width; x++) {
-                if (this.grid[y][x] === 1) {
-                    this.ctx.fillStyle = this.mask[y][x] === 1 ? colorAliveMask : colorAlive;
-
-                } else {
-                    this.ctx.fillStyle = this.mask[y][x] === 1 ? colorDeadMask : colorDead;
-                }
+                this.ctx.fillStyle = this.grid[y][x] === 1 ? colorAlive : colorDead;
                 this.ctx.beginPath();
                 this.ctx.arc(
-                    x * this.cell_size + this.cell_size / 2 + Math.random() - 0.5,
-                    y * this.cell_size + this.cell_size / 2 + Math.random() - 0.5,
-                    this.cell_size / 2,
+                    x * this.cell_size + this.cell_size / 2,
+                    y * this.cell_size + this.cell_size / 2,
+                    Math.floor(this.cell_size / 2),
                     0,
                     2 * Math.PI
                 );
@@ -136,22 +94,18 @@ class GameOfLife {
 }
 
 class MarchingSquares {
-    load(canvas, text) {
-        const dims = getResponsiveDimensions();
+    load(canvas) {
+        const dims = getResponsiveDimensions(canvas);
         this.width = dims.width;
         this.height = dims.height;
         this.cell_size = dims.cellSize;
         this.grid = Array.from({ length: this.height + 2 }, () => Array(this.width + 2).fill(0));
-        this.mask = getTextMask(text.innerHTML || "Hello!", this.width, this.height, this.cell_size);
 
         // Update canvas size
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
         this.canvas.width = this.width * this.cell_size;
         this.canvas.height = this.height * this.cell_size;
-
-        // Set text invisible
-        text.style.color = "#fff0";
 
         // Randomly initialize the grid
         for (let y = 0; y < this.height + 2; y++) {
@@ -169,6 +123,9 @@ class MarchingSquares {
             this.grid[y][0] = 1;
             this.grid[y][this.width + 1] = 1;
         }
+
+        // update once to start with some dead cells
+        this.update();
     }
 
     update() {
@@ -202,33 +159,65 @@ class MarchingSquares {
 
     draw() {
         // Get current theme colors
-        let colorAliveMask = getCSSVariableColor('--accent-color-2') || "darkred";
-        let colorAlive = getCSSVariableColor('--accent-color-1') || "orangered";
-        let colorDeadMask = getCSSVariableColor('--text-color') || "#8888af";
-        let colorDead = getCSSVariableColor('--bg-color-2') || "#eeeef8";
+        let colorDead = getCSSVariableColor('--accent-color-1') || "black";
 
         // Draw cells
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         for (let y = 1; y < this.height + 1; y++) {
             for (let x = 1; x < this.width + 1; x++) {
-                if (this.grid[y][x] === 1) {
-                    this.ctx.fillStyle = this.mask[y - 1][x - 1] === 1 ? colorDeadMask : colorDead;
-                } else {
-                    this.ctx.fillStyle = this.mask[y - 1][x - 1] === 1 ? colorAliveMask : colorAlive;
+                if (this.grid[y][x] === 0) {
+                    this.ctx.fillStyle = colorDead;
+                    this.ctx.beginPath();
+                    this.ctx.rect(
+                        (x + Math.random() / 4) * this.cell_size,
+                        (y + Math.random() / 4) * this.cell_size,
+                        this.cell_size / 2,
+                        this.cell_size / 2
+                    );
+                    this.ctx.fill();
                 }
+            }
+        }
+    }
+}
+
+class Noise {
+    load(canvas) {
+        const dims = getResponsiveDimensions(canvas);
+        this.width = dims.width;
+        this.height = dims.height;
+        this.cell_size = dims.cellSize;
+
+        // Update canvas size
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.canvas.width = this.width * this.cell_size;
+        this.canvas.height = this.height * this.cell_size;
+    }
+
+    update() {
+        this.time += 0.1;
+    }
+
+    draw() {
+        let color1 = getCSSVariableColor('--bg-color-1') || "black";
+        let color2 = getCSSVariableColor('--accent-color-1') || "white";
+
+        this.ctx.globalAlpha = 0.1;
+        this.ctx.fillStyle = color1;
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        // Draw noise
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                let value = Math.random(); // Placeholder for actual Perlin noise function
+                let color = value > 0.5 ? color2 : color1;
+                this.ctx.fillStyle = color;
                 this.ctx.beginPath();
-                // this.ctx.rect(
-                //     x * this.cell_size,
-                //     y * this.cell_size,
-                //     this.cell_size,
-                //     this.cell_size
-                // );
-                this.ctx.arc(
-                    (x - 1) * this.cell_size + this.cell_size / 2 + Math.random() - 0.5,
-                    (y - 1) * this.cell_size + this.cell_size / 2 + Math.random() - 0.5,
-                    this.cell_size / 2,
-                    0,
-                    2 * Math.PI
+                this.ctx.rect(
+                    x * this.cell_size,
+                    y * this.cell_size,
+                    this.cell_size,
+                    this.cell_size
                 );
                 this.ctx.fill();
             }
@@ -238,30 +227,19 @@ class MarchingSquares {
 
 // Initialize the animation when the window loads
 window.onload = function () {
-    const canvas = document.getElementById("animation");
-    const text = document.getElementById("text");
+    const canvas = document.getElementById("banner");
     if (canvas.getContext) {
-        const anim = Math.random() > 0.5 ? new GameOfLife() : new MarchingSquares();
-        anim.load(canvas, text);
-        this.intervalId = setInterval(() => {
+        const anim = Math.random() > 1 / 3 ? (Math.random() > 1 / 2 ? new GameOfLife() : new MarchingSquares()) : new Noise();
+        anim.load(canvas);
+        setInterval(() => {
             anim.update()
             anim.draw();
         }, 100);
 
         addEventListener('resize', () => {
             if (canvas.getContext) {
-                anim.load(canvas, text);
-            }
-        });
-
-        addEventListener('visibilitychange', () => {
-            if (document.hidden) {
-                clearInterval(this.intervalId);
-            } else {
-                this.intervalId = setInterval(() => {
-                    anim.update()
-                    anim.draw();
-                }, 100);
+                anim.load(canvas);
+                anim.draw()
             }
         });
     }
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,27 @@
+function updateToggleButton(button, isDark) {
+    if (button) {
+        button.innerHTML = isDark ? "☀️" : "🌙";
+        button.setAttribute("aria-label", isDark ? "Switch to light mode" : "Switch to dark mode");
+        button.setAttribute("title", isDark ? "Switch to light mode" : "Switch to dark mode");
+    }
+}
+
 function toggleTheme() {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     const newTheme = currentTheme === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", newTheme);
     localStorage.setItem("theme", newTheme);
+    const button = document.getElementById("theme-toggle");
+    updateToggleButton(button, newTheme === "dark");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const savedTheme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
     document.documentElement.setAttribute("data-theme", savedTheme);
+    const button = document.getElementById("theme-toggle");
+    updateToggleButton(button, savedTheme === "dark");
+});
+
+window.addEventListener("load", () => {
+    document.body.classList.remove("preload"); // Flashbang averted
 });
